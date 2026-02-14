ac51ff90b2c19d48a363de044facc96fc35151c7
Jack-Liang <jack.liang.world@gmail.com>
2026-02-14 16:49:43 +0800

    feat: 添加 Cloudflare 适配器并更新依赖
    添加 Cloudflare Workers 适配器配置和相关文件
    更新 Astro 及其插件依赖至最新版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ovidius-astro-theme.netlify.app',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            // Disable injecting a basic `base.css` import on every page.
-            applyBaseStyles: false
-        })
-    ]
+  site: 'https://ovidius-astro-theme.netlify.app',
+
+  integrations: [
+      mdx(),
+      sitemap(),
+      tailwind({
+          // Disable injecting a basic `base.css` import on every page.
+          applyBaseStyles: false
+      })
+  ],
+
+  adapter: cloudflare()
 });
